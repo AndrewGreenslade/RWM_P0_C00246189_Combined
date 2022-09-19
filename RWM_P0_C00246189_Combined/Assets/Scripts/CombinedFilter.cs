c69ff2a28b5630d36feef15687ff52ad5146b6c9
Andrew Greenslade <andrew.greenslade71@yahoo.com>
2022-09-19 11:35:02 +0100

    combined tests and finished lab

---
+++ RWM_P0_C00246189_Combined/Assets/Scripts/CombinedFilter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombinedFilter : MonoBehaviour
+{
+    public static int[] combinedFilter(int[] xs)
+    {
+        return xor21filter.xors(Fact.outputFact(xs));
+    }
+}
---
+++ RWM_P0_C00246189_Combined/Assets/Tests/CombinedFilterTest.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+namespace Tests
+{ 
+    public class CombinedFilterTest
+    {
+        [Test]
+        public void CombinedFilterSimple()
+        {
+            int[] input = { 1,2,3,4,5,6,7 };
+            int[] output = CombinedFilter.combinedFilter(input);
+            int[] expected = { 20,23,19,13,109,709,5029 };
+
+            CollectionAssert.AreEqual(expected, output);
+        }
+    }
+}
